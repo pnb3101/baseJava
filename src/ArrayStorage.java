71b8ed16383164b6b7ace50211a4fb9b1c8fcd75
Nina <n-ina@yandex.ru>
2019-09-01 20:53:18 +0300

    ввела поле int size; изменила код методов класса ArrayStorage, используя поле size.

--- src/ArrayStorage.java
+++ src/ArrayStorage.java
@@ -5,59 +5,53 @@ import java.util.Iterator;
  * Initial resume class
  */
 public class ArrayStorage {
-        Resume[] storage = new Resume[10000];
+    Resume[] storage = new Resume[5];
+    int size = 0;
 
-        void clear() {
-            for(int i=0; i<storage.length; i++) {
-                storage[i] = null;
-            }
+    void clear() {
+        for (int i = 0; i < size; i++) {
+            storage[i] = null;
         }
-
-        void save(Resume r) {
-            int size = size();
-            storage[size] = r;
-        }
-
-        Resume get(String uuid) {
-          Resume result = null;
-          try {
-              for (int i = 0; i < storage.length; i++) {
-                  if (storage[i].uuid.equals(uuid))
-                      result = storage[i];
-              }
-          }catch (NullPointerException e){
-
-          }
-          return result;
-        }
-
-        void delete(String uuid) {
-          try {
-             for (int i = 0; i < storage.length; i++) {
-                 if (storage[i].uuid.equals(uuid))
-                     storage[i] = null;
-                    for(int j=i; j<storage.length-1; j++){
-                        storage[j]=storage[j+1];
-                    }
-                 storage[storage.length-1]=null;
-                 break;
-             }
-         }catch(NullPointerException e){    }
-        }
-
-        Resume[] getAll() {
-          return storage;
-        }
-
-        int size() {
-            int sizestorage=0;
-            try {
-                for (int i = 0; i < storage.length; i++) {
-                    if (!storage[i].equals(null))
-                        sizestorage++;
-                }
-            }catch(NullPointerException e){     }
-            return sizestorage;
-        }
+        size = 0;
     }
+
+    void save(Resume r) {
+        storage[size] = r;
+        size++;
+    }
+
+    Resume get(String uuid) {
+        Resume result = null;
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid))
+                result = storage[i];
+        }
+        return result;
+    }
+
+    void delete(String uuid) {
+        for (int i = 0; i < size; i++) {
+            if (storage[i].uuid.equals(uuid))
+                storage[i] = null;
+            for (int j = i; j < size - 1; j++) {
+                storage[j] = storage[j + 1];
+            }
+            storage[size - 1] = null;
+            break;
+        }
+        size--;
+    }
+
+    Resume[] getAll() {
+        Resume[] listResume = new Resume[size];
+        for (int i = 0; i < size; i++) {
+            listResume[i] = storage[i];
+        }
+        return listResume;
+    }
+
+    int size() {
+        return size;
+    }
+}
 
