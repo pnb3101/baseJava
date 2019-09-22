ce0bbcd1f8ccb4fcd8fdf2a44e437bd7aa9d251a
Nina <n-ina@yandex.ru>
2019-09-22 12:39:55 +0300

    HW 6/ formatted AllStorageTest class

--- test/ru/javawebinar/basejava/storage/AllStorageTest.java
+++ test/ru/javawebinar/basejava/storage/AllStorageTest.java
@@ -4,8 +4,13 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({ArrayStorageTest.class, SortedArrayStorageTest.class,
-        ListStorageTest.class, MapStorageTest.class, MapUuidStorageTest.class
-})
+@Suite.SuiteClasses(
+        {
+                ArrayStorageTest.class,
+                SortedArrayStorageTest.class,
+                ListStorageTest.class,
+                MapStorageTest.class,
+                MapUuidStorageTest.class
+        })
 public class AllStorageTest {
 }
