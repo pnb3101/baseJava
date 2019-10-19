05138a2284ee046d3420614f66fcf846a809f462
Nina <n-ina@yandex.ru>
2019-10-19 18:42:54 +0300

    HW11/ deadlock in MainConcurrency class

--- src/ru/javawebinar/basejava/MainConcurrency.java
+++ src/ru/javawebinar/basejava/MainConcurrency.java
@@ -1,10 +1,27 @@
 package ru.javawebinar.basejava;
 
 public class MainConcurrency {
+    public static final int Thread_num = 10000;
+    public static final int THREAD_NUM = Thread_num;
     private static int counter;
+    private static final Object LOCK = new Object();
+    private static final Object LOCK1 = new Object();
+    private static final Object LOCK2 = new Object();
+    private static int a = 0;
+    private static int b = 0;
+    private static int c;
 
     public static void main(String[] args) throws InterruptedException {
-        System.out.println(Thread.currentThread().getName());
+
+        //deadlock
+        for (int i = 0; i < THREAD_NUM; i++) {
+            for (int j = 0; j < 100; j++) {
+                new Thread(() -> deadLock(a, b)).start();}
+            }
+        System.out.println("a: " + a);
+        System.out.println("b: " + b);
+
+     /*   System.out.println(Thread.currentThread().getName());
         Thread thread0 = new Thread() {
             @Override
             public void run() {
@@ -19,22 +36,44 @@ public class MainConcurrency {
             }
         }).start();
         System.out.println(thread0.getState());
+        final MainConcurrency mainConcurrency = new MainConcurrency();
+        List<Thread> threads = new ArrayList<>(THREAD_NUM);
 
-        for (int i = 0; i < 10000; i++) {
-            new Thread(() -> {
+        for (int i = 0; i < THREAD_NUM; i++) {
+            Thread thread = new Thread(() -> {
                 for (int j = 0; j < 100; j++) {
-                inc();
+                    mainConcurrency.inc();
                 }
-            }).start();
-        }
-        Thread.sleep(1500);
-            System.out.println(counter);
-        }
+            });
+            thread.start();
+            threads.add(thread);
 
-        public static synchronized void inc(){
-            counter++;
+        }
+        threads.forEach(t -> {
+            try {
+                t.join();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        });
+        System.out.println(counter);
+    }
+
+    private static synchronized void inc() {
+        //         synchronized(this) {
+        counter++;
+    */}
+
+    private static void deadLock(int a, int b){
+        synchronized (LOCK1){
+            a++;
+        }
+        synchronized (LOCK2){
+            b=b+a;
         }
     }
+}
+
 
 
 
