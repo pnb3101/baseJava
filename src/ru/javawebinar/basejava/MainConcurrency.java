af2e25cf9f7e019015cb779e0ddc843022cf6108
Nina <n-ina@yandex.ru>
2019-11-17 00:26:24 +0300

    HW12/ Java 8 Stream

--- src/ru/javawebinar/basejava/MainConcurrency.java
+++ src/ru/javawebinar/basejava/MainConcurrency.java
@@ -1,16 +1,29 @@
 package ru.javawebinar.basejava;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
 public class MainConcurrency {
 
     static String d1 = "dead";
     static String d2 = "lock";
+    static int[] arr = {1,2,2,3,4,5,5,5,9};
 
     public static void main(String[] args) throws InterruptedException {
         //deadlock
-        Thread thread1 = new Thread(() -> deadTest(d1, d2));
+       /* Thread thread1 = new Thread(() -> deadTest(d1, d2));
         Thread thread2 = new Thread(() -> deadTest(d2, d1));
         thread1.start();
-        thread2.start();
+        thread2.start();*/
+        System.out.println(minValue(arr));
+        List<Integer> list = new ArrayList<>();
+        list.add(2);
+        list.add(8);
+        list.add(1);
+        list.add(4);
+        System.out.println(oddOrEven(list));
     }
 
     public static void deadTest(String a, String b) {
@@ -25,7 +38,36 @@ public class MainConcurrency {
                 System.out.println("Thread locked b");
             }
         }
+
     }
+    /*
+    реализовать метод через стрим int minValue(int[] values).
+    Метод принимает массив цифр от 1 до 9, надо выбрать уникальные и вернуть минимально возможное число,
+    составленное из этих уникальных цифр. Не использовать преобразование в строку и обратно.
+    Например {1,2,3,3,2,3} вернет 123, а {9,8} вернет 89
+     */
+
+    public static int minValue(int[] values){
+      //  Stream<Integer> stream = (Stream<Integer>) Stream.of(values);
+        int num = 0;
+        int circle = 0;
+        int[] result = IntStream.of(values).distinct().sorted().toArray();
+        for(int i = result.length-1; i>=0; i--){
+                num = num + result[circle] * ((int) Math.pow(10, i));
+                circle++;
+        }
+        return num;
+    }
+    /*
+    реализовать метод List<Integer> oddOrEven(List<Integer> integers) если сумма всех чисел нечетная - удалить все нечетные,
+     если четная - удалить все четные. Сложность алгоритма должна быть O(N). Optional - решение в один стрим
+     */
+    public static List<Integer> oddOrEven(List<Integer> integers){
+        return integers.stream().reduce(0, (a, b) -> a + b)%2 == 0 ?
+                integers.stream().filter(p->p%2!=0).collect(Collectors.toList()) : integers.stream().filter(p->p%2==0).collect(Collectors.toList());
+
+    }
+
 }
 
 
